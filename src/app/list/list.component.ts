5d186ecd33fc7eb985367a71bce90d144db6acbb
TXBIT <shenlong203@gmail.com>
2020-08-12 16:40:50 +0700

    001 Add Some Contents to Components

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  selector: "app-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["./list.component.css"],
 })
 export class ListComponent implements OnInit {
+  characters: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
