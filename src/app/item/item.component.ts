b2ead8fa42bfe1d6e233a5013af78165be315c2b
TXBIT <shenlong203@gmail.com>
2020-08-12 20:53:59 +0700

    009 Creating a Service

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { StarWarsService } from "../star-wars.service";
 
 @Component({
   selector: "app-item",
@@ -15,6 +16,8 @@ export class ItemComponent implements OnInit {
 
   onAssign(side: string) {
     // this.character.side = side;
-    this.sideAssigned.emit({ name: this.character.name, side });
+    // this.sideAssigned.emit({ name: this.character.name, side });
+    const swService = new StarWarsService();
+    swService.onSideChosen({ name: this.character.name, side });
   }
 }
